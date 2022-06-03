14aee1a7a1b91b22f9270b2c236780b470f93b2e
honey0703 <honey0703@gmail.com>
2022-06-03 23:38:09 +0800

    train test split, plot chart

--- HW5/HW5.py
+++ HW5/HW5.py
@@ -1,34 +1,54 @@
-#!/usr/bin/env python
-# coding: utf-8
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+from torch.utils.data import TensorDataset, DataLoader
 
-# In[4]:
-
+import torchvision
+import torchvision.transforms as transforms
 
 import os
 import numpy as np
+import argparse
 
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
-from sklearn.metrics import accuracy_score
+from models import *
+from utils import progress_bar
+import matplotlib.pyplot as plt
 
-
-# ## Load data
-
-# In[5]:
-
+## Load data
+print ("=============== Load Data ===============")
 
 x_train = np.load("x_train.npy")
 y_train = np.load("y_train.npy")
+print(y_train.shape)
+# Train-validate split
+# np.random.seed(2)
+# np.random.shuffle(x_train)
+# np.random.shuffle(y_train)
+x_train, x_val = x_train[:45000,:], x_train[45000:,:]
+y_train, y_val = y_train[:45000,:], y_train[45000:,:]
 
 x_test = np.load("x_test.npy")
 y_test = np.load("y_test.npy")
 
 print(x_train.shape[0], 'train samples')
+print(x_val.shape[0], 'val samples')
 print(x_test.shape[0], 'test samples')
 
-
-# In[6]:
+# Transform to DataLoader
+x_train_tensor = torch.Tensor(x_train).permute(0, 3, 1, 2)
+x_val_tensor = torch.Tensor(x_val).permute(0, 3, 1, 2)
+x_test_tensor = torch.Tensor(x_test).permute(0, 3, 1, 2)
+y_train_tensor = torch.Tensor(y_train).view(-1)
+y_val_tensor = torch.Tensor(y_val).view(-1)
+y_test_tensor = torch.Tensor(y_test).view(-1)
+y_train_tensor = y_train_tensor.type(torch.LongTensor)
+y_val_tensor = y_val_tensor.type(torch.LongTensor)
+y_test_tensor = y_test_tensor.type(torch.LongTensor)
+trainloader = DataLoader(TensorDataset(x_train_tensor, y_train_tensor), batch_size=64)
+valloader = DataLoader(TensorDataset(x_val_tensor, y_val_tensor), batch_size=1)
+testloader = DataLoader(TensorDataset(x_test_tensor, y_test_tensor), batch_size=1)
 
 
 # It's a multi-class classification problem 
@@ -37,104 +57,201 @@ class_index = {'airplane': 0, 'automobile': 1, 'bird': 2, 'cat': 3, 'deer': 4,
 print(np.unique(y_train))
 
 
-# ![image](https://img-blog.csdnimg.cn/20190623084800880.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3lqcDE5ODcxMDEz,size_16,color_FFFFFF,t_70)
+## Build Model (pytorch)
+print ('============ Environment Setting ==========')
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', action='store_true',
+                    help='resume from checkpoint')
+args = parser.parse_args()
 
-# ## Data preprocess
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
-# In[7]:
+print ('================ Build Model =================')
+net = ResNet18()
+net = net.to(device)
+
+if device == 'cuda':
+    net = torch.nn.DataParallel(net)
+    cudnn.benchmark = True
+
+if args.resume:
+    # Load checkpoint.
+    print('==> Resuming from checkpoint..')
+    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+    checkpoint = torch.load('./checkpoint/ckpt.pth')
+    net.load_state_dict(checkpoint['net'])
+    best_acc = checkpoint['acc']
+    start_epoch = checkpoint['epoch']
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=args.lr,
+                      momentum=0.9, weight_decay=5e-4)
+# scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=300)
+scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[300,400,450], gamma=0.1, last_epoch=-1)
+
+# Training
+def train(epoch):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+    hist_train.append(train_loss/(batch_idx+1))
+    
+    return hist_train
+        
+# Validating
+def valid(epoch):
+    global best_acc
+    net.eval()
+    val_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(valloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
+            val_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            progress_bar(batch_idx, len(valloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                         % (val_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            
+        hist_val.append(val_loss/(batch_idx+1))
+            
+    # Save checkpoint.
+    acc = 100.*correct/total
+    if acc > best_acc:
+        print('Saving..')
+        state = { 
+            'net': net.state_dict(),
+            'acc': acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, './checkpoint/ckpt.pth')
+        best_acc = acc
+        
+    return hist_val
+
+# Testing
+def test():
+    global best_acc
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    
+
+hist_train = []
+hist_val = []
+for epoch in range(start_epoch, start_epoch+500):
+    hist_train = train(epoch)
+    hist_val = valid(epoch)
+    test()
+    scheduler.step()
+
+# Plot loss chart
+plt.figure(figsize=(10,5))
+plt.title("Training and Validation Loss")
+plt.plot(hist_val,label="val")
+plt.plot(hist_train,label="train")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend()
+plt.savefig("Loss Chart.png")
 
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-
-# Convert class vectors to one-hot encoding (keras model requires one-hot label as inputs)
-num_classes = 10
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
 
 
-# ## Build model & training (Keras)
-
-# In[9]:
 
 
-import warnings
-warnings.filterwarnings('ignore')
-# Builde model
-model = Sequential() # Sequential groups a linear stack of layers 
-model.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=x_train.shape[1:])) # Add Convolution layers
-model.add(Activation('relu')) # Add Relu activation for non-linearity
-model.add(Conv2D(filters=32, kernel_size=(3, 3))) # Add Convolution layers
-model.add(Activation('relu')) # Add Relu activation for non-linearity
-model.add(MaxPooling2D(pool_size=(4, 4))) # Add Max pooling to lower the sptail dimension
-
-model.add(Flatten()) # Flatten the featuremaps
-model.add(Dense(units=512)) # Add dense layer with 512 neurons
-model.add(Activation('relu')) # Add Relu activation for non-linearity
-model.add(Dense(units=num_classes)) # Add final output layer for 10 classes
-model.add(Activation('softmax')) # Add softmax activation 
-
-# initiate SGD optimizer
-opt = keras.optimizers.SGD()
-
-# Compile the model with loss function and optimizer
-model.compile(loss='categorical_crossentropy',optimizer=opt,metrics=['accuracy'])
 
 
-batch_size = 32
-epochs = 10
-# Fit the data into model
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_data=(x_test, y_test),
-          shuffle=True)
 
 
-# In[10]:
 
 
-y_pred = model.predict(x_test)
-print(y_pred.shape)
+
+# # In[10]:
 
 
-# In[11]:
+# y_pred = model.predict(x_test)
+# print(y_pred.shape)
 
 
-y_pred[0]
+# # In[11]:
 
 
-# In[12]:
+# y_pred[0]
 
 
-np.argmax(y_pred[0])
+# # In[12]:
 
 
-# In[13]:
+# np.argmax(y_pred[0])
 
 
-y_pred = np.argmax(y_pred, axis=1)
+# # In[13]:
 
 
-# ## DO NOT MODIFY CODE BELOW!
-# please screen shot your results and post it on your report
-
-# In[14]:
+# y_pred = np.argmax(y_pred, axis=1)
 
 
-assert y_pred.shape == (10000,)
+# # ## DO NOT MODIFY CODE BELOW!
+# # please screen shot your results and post it on your report
+
+# # In[14]:
 
 
-# In[15]:
+# assert y_pred.shape == (10000,)
 
 
-y_test = np.load("y_test.npy")
-print("Accuracy of my model on test-set: ", accuracy_score(y_test, y_pred))
+# # In[15]:
 
 
-# In[ ]:
+# y_test = np.load("y_test.npy")
+# print("Accuracy of my model on test-set: ", accuracy_score(y_test, y_pred))
+
+
+# # In[ ]:
 
 
 
